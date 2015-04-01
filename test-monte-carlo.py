bba14a67b5106f2a5d20590e73ad7a2c386ddf29
Fox <sleepyfox@gmail.com>
2015-04-01 16:27:07 +0100

    added test for picking random item from list

--- test-monte-carlo.py
+++ test-monte-carlo.py
@@ -1,14 +1,26 @@
 "A test suite for a Monte-Carlo simulator"
 
 import unittest
+import random
+
+class WorkItemList():
+	list = [1]
+
+	def pickItemFromList(self):
+		return random.choice(self.list)
 
 class TestSuite(unittest.TestCase):
 	def setUp(self):
 		self.monte = 1
 
-	def test1isI(self):
-		"1 is equal to 1"
+	def testFileProcessorCanParseData(self):
+		"A single line is parsed into a..."
 		self.assertEqual(self.monte, 1)
+
+	def testPickOneFromListofOneItem(self):
+		"When we pick an item from a list of one, we always get the same item"
+		myList = WorkItemList()
+		self.assertEqual(myList.pickItemFromList(), 1)
 
 if __name__ == "__main__":
 	unittest.main()
