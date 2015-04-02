133c07d745aa3cf4bf015699228fca5efcc2b98a
Fox <sleepyfox@gmail.com>
2015-04-02 14:40:36 +0100

    added test for analyser returning a map of lists

--- monte_carlo.py
+++ monte_carlo.py
@@ -1,31 +1,36 @@
 import random
 
 ALL_BUT_LAST = -1
+COMMA = ','
+NEWLINE = '\n'
 
 def analyse(project_stats):
 	"""Takes a list of pairs of (Story points, time in minutes)
 	   Returns a map of lists of time in minutes grouped by
 	   (small|medium|large)"""
-	return { 'small': 1, 'medium': 1, 'large': 1}
+	return { 'small': [], 'medium': [], 'large': []}
 
 class WorkItemList():
 	def __init__(self, xs):
 		self.list = xs
 
 	def pickItemFromList(self):
-		"picks a single work item at random from a list"
+		'picks a single work item at random from a list'
 		return random.choice(self.list)
 
 	def sample(self, times):
-		"returns a series of random samples from a list"
+		'returns a series of random samples from a list'
 		return [self.pickItemFromList() for i in range(times)]
 
 
 class ProjectFileReader():
 	def __init__(self, filename):
+		'''Takes a CSV file name, reads it in and
+		   Returns a list of pairs of numbers'''
+
 		def parseWorkItem(line_of_text):
-			return list(map(int, line_of_text.split(',')))
+			return list(map(int, line_of_text.split(COMMA)))
 
 		with open(filename) as f:
-			lines = f.read().split('\n')[:ALL_BUT_LAST]
+			lines = f.read().split(NEWLINE)[:ALL_BUT_LAST]
 			self.lines = [parseWorkItem(line) for line in lines]
--- test-monte-carlo.py
+++ test-monte-carlo.py
@@ -1,63 +1,70 @@
-"A test suite for a Monte-Carlo simulator"
+'A test suite for a Monte-Carlo simulator'
 
 import unittest
 from monte_carlo import *
 
 ESTIMATES = {
-  "small": 7,
-  "medium": 5,
-  "large": 4
+  'small': 7,
+  'medium': 5,
+  'large': 4
 }
-
 
 class ListSamplerSuite(unittest.TestCase):
 	def test_pick_from_list_of_one_item(self):
-		"When we pick an item from a list of one, we always get the same item"
+		'When we pick an item from a list of one, we always get the same item'
 		myList = WorkItemList([1])
 		self.assertEqual(myList.pickItemFromList(), 1)
 
 	def test_sample_of_ten_has_ten_items(self):
-		"When we ask for ten samples from a list then we get 10 results"
+		'When we ask for ten samples from a list then we get 10 results'
 		myList = WorkItemList([2])
 		self.assertEqual(len(myList.sample(10)), 10)
 
 	def test_sample_of_ten_has_items_all__the_same(self):
-		"When we ask for 10 samples from a list of one then we get 10 items all the same"
+		'When we ask for 10 samples from a list of one then we get 10 items all the same'
 		myList = WorkItemList([3])
 		self.assertEqual(myList.sample(10).count(3), 10)
 
 
 class FileReaderSuite(unittest.TestCase):
 	def test_that_test_file_should_have_one_line(self):
-		"When we try and read a small test file we get one row"
+		'When we try and read a small test file we get one row'
 		myProjectData = ProjectFileReader("small-test-file.csv")
 		self.assertEqual(len(myProjectData.lines), 1)
 
 	def test_project_data_has_story_point_size(self):
-		"From a single row test data file, we have a numerical Story Point"
+		'From a single row test data file, we have a numerical Story Point'
 		myProjectData = ProjectFileReader("small-test-file.csv")
 		self.assertEqual(myProjectData.lines[0][0], 25)
 
 	def test_project_data_has_story_time_value(self):
-		"From a single row test data file, we have a time value associated with a Story Point"
-		myProjectData = ProjectFileReader("small-test-file.csv")
+		'From a single row test data file, we have a time value associated with a Story Point'
+		myProjectData = ProjectFileReader('small-test-file.csv')
 		self.assertEqual(myProjectData.lines[0][1], 376200)
 
 	def test_that_real_file_should_have_259_lines(self):
-		"When we try and read a small test file we get one row"
-		myProjectData = ProjectFileReader("project-stats.csv")
+		'When we try and read a small test file we get one row'
+		myProjectData = ProjectFileReader('project-stats.csv')
 		self.assertEqual(len(myProjectData.lines), 259)
 
 
 class ProjectAnalyserSuite(unittest.TestCase):
-	def test_empty_work_list_has_correct_categories(self):
-		"Given an empty list of project data, work distribution has all three categories"
-		myProjectData = ProjectFileReader("project-stats.csv")
+	def test_empty_work_list_analysis_has_correct_categories(self):
+		'An analysis of project data should have all three categories'
+		myProjectData = ['25,376200']
 		myProjectAnalysis = analyse(myProjectData)
 		self.assertIn('small', myProjectAnalysis)
 		self.assertIn('medium', myProjectAnalysis)
 		self.assertIn('large', myProjectAnalysis)
 
+	def test_analyser_categories_have_empty_lists(self):
+		'An analysis of project data should retuen empty lists for all three categories'
+		myProjectData = ['25,376200']
+		myProjectAnalysis = analyse(myProjectData)
+		self.assertIsInstance(myProjectAnalysis['small'], list)
+		self.assertIsInstance(myProjectAnalysis['small'], list)
+		self.assertIsInstance(myProjectAnalysis['small'], list)
 
+	# def test_analyser
 if __name__ == "__main__":
 	unittest.main()
